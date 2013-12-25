c29757e185441a1722aaa6f00d1027defcdd1f0f
Daniel Silver <das2c3@mail.missouri.edu>
2013-12-25 13:00:26 -0600

    Rounding standards for usd and btc

--- trader.rb
+++ trader.rb
@@ -9,6 +9,8 @@ class Trader
     @db = DatabaseHandler.new
     @coinbase = Coinbase::Client.new ENV['COINBASE_API_KEY']
 
+    @values = []
+
     @coinbase_flat_fee = 0.15
     @coinbase_percentage_fee = 0.01
 
@@ -16,12 +18,22 @@ class Trader
     @min_percent_drop = min_percent_drop_for_purchase
   end
 
+  def addPrice(price)
+    @values << price
+    @values.shift if @values.length > 80
+    @max = @values.max
+  end
+
   def trade
     loop do
+      #record the current purchase price
+      @one_btc_price_with_fee = @coinbase.buy_price 1
+      addPrice @one_btc_price_with_fee
+
       #switch between purchasing and selling
       @db.last_transaction[:type] == :purchase ? consider_sale : consider_purchase
       puts "\n", "*".light_cyan*70, "\n"
-      sleep 20
+      sleep 60
     end
   end
 
@@ -32,16 +44,16 @@ class Trader
   def consider_sale
     last_purchase = @db.last_transaction "purchase"
     @sell_price = @coinbase.sell_price(bitcoin_balance).to_f
-    puts "\nLast purchase was #{last_purchase[:quantity].round 4} BTC for $#{last_purchase[:price].round 4}".light_green
-    puts "Can sell #{bitcoin_balance.round 4} BTC for $#{@sell_price.round 4}".light_blue
+    puts "\nLast purchase was #{last_purchase[:quantity].btc_round} BTC for $#{last_purchase[:price].usd_round}".light_green
+    puts "Can sell #{bitcoin_balance.btc_round} BTC for $#{@sell_price.usd_round}".light_blue
     #calculate sale profit
     profit = @sell_price - last_purchase[:price]
     min_profit = last_purchase[:price] * @min_percent_gain
     if profit >= min_profit
-      puts "Profit $#{profit.round 2} >= $#{min_profit.round 2}".green
+      puts "Profit $#{profit.usd_round} >= $#{min_profit.usd_round}".green
       sell
     else
-      puts "Profit $#{profit.round 2} < $#{min_profit.round 2}".light_red
+      puts "Profit $#{profit.usd_round} < $#{min_profit.usd_round}".light_red
     end
   end
 
@@ -53,27 +65,29 @@ class Trader
 
   def consider_purchase
     last_sale = @db.last_transaction "sale"
-    buy_price = @coinbase.buy_price 1
+    buy_price = @one_btc_price_with_fee
     one_btc_price = (buy_price.to_f - @coinbase_flat_fee) / @coinbase_percentage_fee
-    #puts "buy price: $#{buy_price}"
     available_funds = last_sale[:price].to_f
+
+    average = @values.inject{ |sum, el| sum + el }.to_f / @values.size
+    puts "average: #{average.round 2}".magenta + "  current: #{@one_btc_price_with_fee}".red + "  max: #{@max}".light_cyan
+    percent_change = (@one_btc_price_with_fee - @max) / @max
+    puts "percent_change: #{(percent_change*100).round 4}%"
 
     btc_to_purchase = ((available_funds - @coinbase_flat_fee) / @coinbase_percentage_fee) / one_btc_price
 
-    last_sale_dollar_val = last_sale[:price].round 4
-    puts "Last sale was #{last_sale[:quantity].round 4} BTC for $#{last_sale_dollar_val}".light_green
-    min_amount_to_purchase = last_sale[:quantity] * (1 + @min_percent_drop)
-    puts "Can buy #{btc_to_purchase.round 6} BTC for $#{available_funds.round 2}".blue
-    if btc_to_purchase >= min_amount_to_purchase
-      puts "BTC to purchase #{btc_to_purchase.round 6} BTC >= min of #{min_amount_to_purchase.round 4} BTC".green
+    puts "Last sale was #{last_sale[:quantity].btc_round} BTC for $#{last_sale[:price].usd_round}".light_green
+
+    puts "Can buy #{btc_to_purchase.btc_round} BTC for $#{available_funds.usd_round}".blue
+
+    if percent_change <= -@min_percent_drop
       purchase btc_to_purchase, available_funds
-    else
-      puts "BTC to purchase #{btc_to_purchase.round 6} BTC < min of #{min_amount_to_purchase.round 4} BTC".light_red
     end
   end
 
   def purchase(btc, cost)
     puts "BUYING!".green
+    puts @coinbase.buy! btc
     @db.insert_transaction cost, btc, :purchase
   end
 
@@ -84,5 +98,14 @@ class Trader
       cost = /\$(?<amount>\d+\.*\d*)/.match(t.transaction.notes)[:amount].to_f
       @db.insert_transaction cost, t.transaction.amount.to_f.abs, type
     end
+  end
+end
+
+class Float
+  def btc_round
+    self.round 6
+  end
+  def usd_round
+    self.round 2
   end
 end
